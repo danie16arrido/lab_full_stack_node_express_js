665b8c5dba7bbfbbc2ed84191993b3b6d8c8979e
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-06-28 15:01:31 +0100

    Complete REST

--- controllers/myCountries.js
+++ controllers/myCountries.js
@@ -12,7 +12,32 @@ var sourceCountries = [
 
 //country index
 countryRouter.get('/', function(req, res) {
+  res.json( sourceCountriesB );
+});
+
+countryRouter.get('/:id', function (req, res) {
+  res.json( sourceCountriesB[ req.params.id ])
+});
+
+countryRouter.post('/', function (req, res) {
+  var country = {
+    name: req.body.name
+  }
+  sourceCountries.push(country);
   res.json( sourceCountries );
-})
+});
+
+countryRouter.put('/:id', function(req, res) {
+  var updatedCountry = {
+    name: req.body.name
+  }
+  sourceCountries[req.params.id] = updatedCountry;
+  res.json( sourceCountries );
+});
+
+countryRouter.delete('/:id', function(req, res) {
+  sourceCountries.splice(req.params.id, 1);
+  res.json( sourceCountries );
+});
 
 module.exports = countryRouter;
